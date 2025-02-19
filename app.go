2a6d7c9ff3cdf969a94ef2f43cdb440e520b8756
erfuuan <erfuuan@gmail.com>
2025-02-20 00:45:29 +0330

    first push

---
+++ app.go
@@ -0,0 +1,58 @@
+
+package main
+import "fmt"
+
+// func main() {
+//      name := "Erfuuan"
+//      age := 25
+//     fmt.Println(name, "is", age, "years old")
+// }
+//?=====================================================================
+// func main() {
+// 	if age==18 {
+// 		fmt.Println("bache")
+// 	}else{
+// 		fmt.Println("bozorg")
+// 	}
+// }
+//?=====================================================================
+
+// func main() {
+// 	for i:=1; i<=5; i++ {
+// 		fmt.Println("level", i)	
+// 	}
+// }
+
+// func main(){
+// 	for {
+// 		fmt.Println("infinity")
+// 	}
+// }
+//?=====================================================================
+// func greet(name string){fmt.Println("hi :", name)}
+// func add (a int , b int) int{return a+b}
+
+// func main(){
+// 	greet("erfan")
+// 	result:=add(3,4)
+// 	fmt.Println(result)
+// }
+//?=====================================================================
+// type Person struct {
+// 	Name string 
+// 	Age int
+// }
+// func main(){
+//     p := Person{Name: "Erfuuan", Age: 25}
+// 	fmt.Println(p)
+// }
+//?=====================================================================
+// func sayHi (){
+// 	fmt.Println("hi")
+// }
+
+// func main(){
+// 	go sayHi()
+// 	fmt.Println("goodBye")
+// }
+//?=====================================================================
